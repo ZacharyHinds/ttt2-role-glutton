baf3744b2897bc220c6a889f35a7a0f543420742
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-06-28 23:58:49 -0400

    Base file and weapon creation

---
+++ lua/lang/english/glutton.lua
@@ -0,0 +1,13 @@
+L = LANG.GetLanguageTableReference("english")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[GLUTTON.name] = "Glutton"
+L["info_popup_" .. GLUTTON.name] = [[You are a Glutton!
+Feed or Starve!]]
+L["body_found_" .. GLUTTON.abbr] = "They were a Glutton!"
+L["search_role_" .. GLUTTON.abbr] = "This person was a Glutton!"
+L["target_" .. GLUTTON.name] = "Glutton"
+L["ttt2_desc_" .. GLUTTON.name] = [[The Glutton is a Traitor who can feast on the living and the dead to fight their growing hunger.
+As your hunger grows, so does your strength. But be careful, if you let it consume you'll need to eat everyone to survive!]]
+
+-- OTHER ROLE LANGUAGE STRINGS
