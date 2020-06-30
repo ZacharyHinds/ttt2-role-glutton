7dce07e62d634d22c3c708003842d972b378feaa
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-06-30 01:19:01 -0400

    Created HUD element

---
+++ lua/terrortown/gamemode/shared/hud_elements/ttthunger/old_ttt_hunger.lua
@@ -0,0 +1,126 @@
+local base = "old_ttt_element"
+
+DEFINE_BASECLASS(base)
+
+HUDELEMENT.Base = base
+
+if CLIENT then -- CLIENT
+	local const_defaults = {
+		basepos = {x = 0, y = 0},
+		size = {w = 0, h = 45},
+		minsize = {w = 0, h = 45}
+	}
+
+	function HUDELEMENT:PreInitialize()
+		BaseClass.PreInitialize(self)
+
+		hudelements.RegisterChildRelation(self.id, "old_ttt_info", false)
+	end
+
+	function HUDELEMENT:Initialize()
+		BaseClass.Initialize(self)
+	end
+
+	function HUDELEMENT:GetDefaults()
+		local height = 45
+		local parent = self:GetParentRelation()
+		local parentEl = hudelements.GetStored(parent)
+		local x, y = 15, ScrH() - height - self.maxheight - self.margin
+
+		if parentEl then
+			x = parentEl.pos.x
+			y = parentEl.pos.y - self.margin - height - 30
+		end
+
+		const_defaults["basepos"] = {x = x, y = y}
+		const_defaults["size"] = {w = self.maxwidth, h = 45}
+		const_defaults["minsize"] = {w = self.maxwidth, h = 45}
+
+		return const_defaults
+	end
+
+	function HUDELEMENT:DrawComponent(name, col, val, multiplier)
+		multiplier = multiplier or 1
+
+		local pos = self:GetPos()
+		local size = self:GetSize()
+		local x, y = pos.x, pos.y
+		local width, height = size.w, size.h
+
+		draw.RoundedBox(8, x, y, width, height, self.bg_colors.background_main)
+
+		local bar_width = width - self.dmargin
+		local bar_height = height - self.dmargin
+
+		local tx = x + self.margin
+		local ty = y + self.margin
+
+		self:PaintBar(tx, ty, bar_width, bar_height, col, multiplier)
+
+		if val then
+			self:ShadowedText(val, "HealthAmmo", tx + bar_width * 0.5, ty + bar_height * 0.5, COLOR_WHITE, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		end
+
+		draw.SimpleText(name, "TabLarge", x + self.margin * 2, y, COLOR_WHITE, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+	end
+
+	local edit_colors = {
+		border = COLOR_WHITE,
+		background = Color(0, 0, 10, 200),
+		fill = Color(100, 100, 100, 255)
+	}
+
+	function HUDELEMENT:Draw()
+		local ply = LocalPlayer()
+
+		if not IsValid(ply) then return end
+
+		local multiplier
+
+		local color = GLUTTON.color
+		if not color then return end
+
+		if ply:IsActive() and ply:Alive() and ply:GetSubRole() == ROLE_GLUTTON then
+			if not ply:GetNWInt("Appetite", 0) > 0 then
+				local hungerTime = ply:GetNWInt("Hunger", 0)
+				local appetite_state = client:GetNWInt("Appetite", 0)
+        local delay = 1
+        local appetite_name = "none"
+
+        if appetite_state == 1 then
+          delay = 30
+        elseif appetite_state == 2 then
+          delay = 20
+        elseif appetite_state == 3 then
+          delay = 10
+        elseif appetite_state == 4 then
+          delay = 1
+        end
+
+				multiplier = bloodlustTime - CurTime()
+				multiplier = multiplier / delay
+
+				local secondColor = VAMPIRE.bgcolor
+				local r = color.r - (color.r - secondColor.r) * multiplier
+				local g = color.g - (color.g - secondColor.g) * multiplier
+				local b = color.b - (color.b - secondColor.b) * multiplier
+
+				color = Color(r, g, b, 255)
+			else
+				multiplier = 0
+			end
+		end
+
+		if HUDEditor.IsEditing then
+			self:DrawComponent("Hunger", edit_colors, "Hunger!")
+		elseif multiplier then
+			local col_tbl = {
+				border = COLOR_WHITE,
+				background = self.bg_colors.background_main,
+				fill = color
+			}
+
+			self:DrawComponent("Hunger", col_tbl, ply:GetNWInt("Appetite", 0) and "Hunger!", multiplier)
+		end
+	end
+end
